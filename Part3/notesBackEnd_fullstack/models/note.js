551ca396ef1835ebf79315fd2facb4a374afd224
Björn Carlsson <bjorn.carlsson@raceid.com>
2025-02-03 20:38:23 +0100

    functional blogPOST/GET part 4.1

--- Part3/notesBackEnd_fullstack/models/note.js
+++ Part3/notesBackEnd_fullstack/models/note.js
@@ -1,27 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-
-const url = process.env.MONGODB_URI
-
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
     minLength: 3,
-    required: true
+    required: true,
   },
   important: Boolean,
 })
@@ -31,8 +14,7 @@ noteSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  }
+  },
 })
-
 
 module.exports = mongoose.model('Note', noteSchema)
